4c0498e713a2e9c4fdc85cc36cdbb047e79ed28d
lixel_ <133605174+lixelv@users.noreply.github.com>
2025-04-07 16:10:42 +0300

    Added 7 sidebar in project

--- src/lib/region/table/table.svelte
+++ src/lib/region/table/table.svelte
@@ -44,39 +44,39 @@
 	class:!grid-cols-1={$filtered.length === 0}
 >
 	<!-- {#if $isOwner} -->
-		<Dialog.Root bind:open={$regionDialogOpen}>
-			<Dialog.Trigger>
-				{#snippet child({ props })}
-					<Button
-						{...props}
-						class="relative h-[130px] w-full text-center max-sm:h-[80px] sm:col-auto"
-						variant="outline"
-					>
-						Create region
-						<PlusCircle class={icon.left} />
-					</Button>
-				{/snippet}
-			</Dialog.Trigger>
-			<Dialog.Content class="sm:max-w-[425px]" trapFocus={false}>
-				<Dialog.Header>
-					<Dialog.Title>Create region</Dialog.Title>
-					<Dialog.Description>Enter your new region name.</Dialog.Description>
-				</Dialog.Header>
-				<form
-					class="flex gap-2"
-					on:submit|preventDefault={toastApi.execAsync(
-						async () => await createRegion($regionForm),
-						`Region ${$regionForm.name} created.`,
-						`Failed to create region ${$regionForm.name}, my be this region already exists.`
-					)}
+	<Dialog.Root bind:open={$regionDialogOpen}>
+		<Dialog.Trigger>
+			{#snippet child({ props })}
+				<Button
+					{...props}
+					class="relative h-[130px] w-full text-center max-sm:h-[80px] sm:col-auto"
+					variant="outline"
 				>
-					<div class="grid w-full grid-cols-1 gap-2">
-						<Input id="name" placeholder="Region name" bind:value={$regionForm.name} required />
-					</div>
-					<Button type="submit" class="h-full">Create <PlusCircle class={icon.left} /></Button>
-				</form>
-			</Dialog.Content>
-		</Dialog.Root>
+					Create region
+					<PlusCircle class={icon.left} />
+				</Button>
+			{/snippet}
+		</Dialog.Trigger>
+		<Dialog.Content class="sm:max-w-[425px]" trapFocus={false}>
+			<Dialog.Header>
+				<Dialog.Title>Create region</Dialog.Title>
+				<Dialog.Description>Enter your new region name.</Dialog.Description>
+			</Dialog.Header>
+			<form
+				class="flex gap-2"
+				on:submit|preventDefault={toastApi.execAsync(
+					async () => await createRegion($regionForm),
+					`Region ${$regionForm.name} created.`,
+					`Failed to create region ${$regionForm.name}, my be this region already exists.`
+				)}
+			>
+				<div class="grid w-full grid-cols-1 gap-2">
+					<Input id="name" placeholder="Region name" bind:value={$regionForm.name} required />
+				</div>
+				<Button type="submit" class="h-full">Create <PlusCircle class={icon.left} /></Button>
+			</form>
+		</Dialog.Content>
+	</Dialog.Root>
 	<!-- {/if} -->
 	{#each $filtered as region (region.id)}
 		<Card.Root class="relative col-[1/-1] h-[130px] sm:col-auto">
--- src/routes/[team]/~/settings/+page.svelte
+++ src/routes/[team]/~/settings/+page.svelte
@@ -1,6 +1,25 @@
 <script lang="ts">
-	import { TeamName } from '$lib/team/settings/team-name';
-	import { LeaveTeam } from '$lib/team/settings/leave-team';
+	import { teamStore, havePermission } from '$lib/stores.js';
+	import { goto } from '$app/navigation';
+	import type { TeamsResponse } from '$lib/types';
+	import { writable } from 'svelte/store';
+	import { pb } from '$lib/pocketbase/client';
+
+	import * as Card from '$lib/components/ui/card';
+	import { Input } from '$lib/components/ui/input';
+	import { Button } from '$lib/components/ui/button/index.js';
+
+	const teamNameChangeValue = writable<string>($teamStore?.name);
+
+	const teamNameChangeSubmit = async () => {
+		const result = await pb.collection('teams').update<TeamsResponse>($teamStore?.id as string, {
+			...$teamStore,
+			name: $teamNameChangeValue
+		});
+
+		teamStore.set(result);
+		goto('/' + $teamNameChangeValue + '/~/settings');
+	};
 </script>
 
 <div class="mb-6 w-[100vw] border-b border-input py-10 text-3xl font-medium">
@@ -8,6 +27,28 @@
 </div>
 
 <div class="mx-auto grid w-[min(748px,100vw)] grid-cols-1 gap-6 px-6">
-	<TeamName />
-	<LeaveTeam />
+	<!-- Edit Name -->
+	<Card.Root class="w-[min(max(240px,80vw),600px)]" muted={true}>
+		<Card.Header>
+			<Card.Title>Team Name</Card.Title>
+			<Card.Description>
+				This is your team's name and url in ControlHub. For example name of your school or
+				university.
+			</Card.Description>
+		</Card.Header>
+
+		<form on:submit|preventDefault={teamNameChangeSubmit}>
+			<Card.Content>
+				<Input
+					bind:value={$teamNameChangeValue}
+					oninput={() => teamNameChangeValue.set($teamNameChangeValue.slice(0, 32))}
+				/>
+			</Card.Content>
+
+			<Card.Footer class="flex items-center justify-between border-t px-6 py-4">
+				<p class="text-sm text-muted-foreground">Please use 32 characters at maximum.</p>
+				<Button type="submit">Save</Button>
+			</Card.Footer>
+		</form>
+	</Card.Root>
 </div>
